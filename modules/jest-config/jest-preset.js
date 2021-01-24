39454835910aa328cd6d52ae0f69c2257f7daf9c
Eldar Shamukhamedov <shamu1001@gmail.com>
2021-01-24 11:32:28 -0500

    Add basic CircleCI deploy pipeline

--- modules/jest-config/jest-preset.js
+++ modules/jest-config/jest-preset.js
@@ -1,13 +1,15 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/source/**/*.{js,mjs,jsx,ts,tsx}',
-    '!<rootDir>/source/**/__tests__/**/*.{js,mjs,jsx,ts,tsx}',
+    '<rootDir>/source/**/*.{js,mjs,cjs,jsx,ts,tsx}',
+    '!<rootDir>/source/**/__tests__/**/*.{js,mjs,cjs,jsx,ts,tsx}',
   ],
   modulePaths: ['<rootDir>/', '<rootDir>/source'],
   moduleFileExtensions: ['js', 'mjs', 'jsx', 'ts', 'tsx'],
   setupFilesAfterEnv: ['@aperture.io/jest-config/setupTests.js'],
-  testMatch: ['<rootDir>/source/**/__tests__/**/*.test.{js,mjs,jsx,ts,tsx}'],
+  testMatch: [
+    '<rootDir>/source/**/__tests__/**/*.test.{js,mjs,cjs,jsx,ts,tsx}',
+  ],
   testURL: 'http://localhost:8080',
   transform: {
     '^.+\\.(js|mjs|jsx|json)$': 'babel-jest',
